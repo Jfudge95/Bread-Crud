e888a0d715f55a7f83f54b4a2650535b24d70d50
Jordan Fudge <jordanfudge95@gmail.com>
2023-05-14 18:55:39 -0400

    Added Notes for future studying of code/db

--- models/bread.js
+++ models/bread.js
@@ -17,7 +17,7 @@ const breadSchema = new mongoose.Schema({
 module.exports = mongoose.model("Bread", breadSchema);
 
 // MVC is our Models, Views, and our Controllers.
-// Models is abotu our data and how it's structured
+// Models is about our data and how it's structured
 // Views is what our users see and interact with
 // Controllers combine the models and Views and handles the interaction between these 2.
 
